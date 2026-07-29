ee3293685ee38bf09f35caabf297103335234e63
Rober Maher <robermaher@gmail.com>
2026-07-29 20:15:12 +0300

    Replace logo PNG with SVG in CustomAppBar and update imports

--- verve/lib/features/auth/widgets/custom_app_bar.dart
+++ verve/lib/features/auth/widgets/custom_app_bar.dart
@@ -1,7 +1,9 @@
 // features/auth/widgets/custom_app_bar.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:verve/core/constants/app_colors.dart';
 import 'package:verve/features/auth/widgets/custom_text_button.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -10,11 +12,17 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      automaticallyImplyLeading: false,
       backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
       title: Row(
         children: [
-          Image.asset('assets/images/logo.png', height: 64.h, width: 200.w),
+          SvgPicture.asset(
+            'assets/images/logo.svg',
+            colorFilter: ColorFilter.mode(
+              AppColors.primaryColor,
+              BlendMode.srcIn,
+            ),
+          ),
           const Spacer(),
           CustomTextButton(text: 'Select Country', ontap: () {}),
           Gap(5.w),
